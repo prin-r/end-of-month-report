a467f5972f440545b8cc8a5d81a45ad846775198
prin-r <q@bandprotocol.com>
2021-05-06 14:43:42 +0700

    -

---
+++ index.js
@@ -0,0 +1,196 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+const Web3 = require("web3");
+const web3 = new Web3("https://rpcapi.fantom.network");
+
+// http://std-price.d3n.xyz//v1/graphql
+// http://feeder-graphql.bandchain.org/v1/graphql
+
+const network = "mainnet_target_mirror";
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+function zip(arrays) {
+  return arrays[0].map(function (_, i) {
+    return arrays.map(function (array) {
+      return array[i];
+    });
+  });
+}
+
+txNotFound = [];
+
+async function getTxInput(created_at, txHash) {
+  try {
+    const tx = await web3.eth.getTransaction(txHash);
+    const r = web3.eth.abi.decodeParameters(
+      ["string[]", "uint64[]", "uint64[]", "uint64[]"],
+      "0x" + tx["input"].slice(10)
+    );
+    return zip([r["0"], r["1"], r["2"], r["3"]]);
+  } catch (e) {
+    console.log("get: ", txHash, " : fail");
+    txNotFound = [...txNotFound, [created_at, txHash]];
+  }
+  return [];
+}
+
+async function getTxInputTerra(created_at, txHash) {
+  let i = 0;
+  while (i < 10) {
+    try {
+      const result = await fetch("https://fcd.terra.dev/v1/tx/" + txHash);
+      const {
+        tx: {
+          value: { msg },
+        },
+      } = await result.json();
+
+      let acc = [];
+      for (let i = 0; i < msg.length; i++) {
+        const {
+          relay: { symbols, rates, resolve_times, request_ids },
+        } = JSON.parse(
+          Buffer.from(msg[i]["value"]["execute_msg"], "base64").toString(
+            "utf-8"
+          )
+        );
+        const z = zip([symbols, rates, resolve_times, request_ids]);
+        acc = [...acc, ...z];
+      }
+
+      if (i !== 0) {
+        console.log("recover:", txHash);
+      }
+
+      return acc;
+    } catch (e) {
+      console.log("get: ", txHash, " : fail ", i);
+      // txNotFound = [...txNotFound, [created_at, txHash]];
+    }
+    i++;
+    await sleep(1000);
+  }
+  return [];
+}
+
+async function fetchGraphQL(offset) {
+  const result = await fetch("http://std-price.d3n.xyz//v1/graphql", {
+    method: "POST",
+    body: JSON.stringify({
+      query: `
+      query MyQuery {
+        ${network}_relay_tx(limit: 1000, order_by: {created_at: desc}, offset: ${offset}) {
+          created_at
+          tx_hash
+        }
+      }
+    `,
+      variables: {},
+      operationName: "MyQuery",
+    }),
+  });
+
+  const { errors, data } = await result.json();
+  if (errors) {
+    console.error(errors);
+    return null;
+  }
+  return data[`${network}_relay_tx`];
+}
+
+const graphqlToJson = async () => {
+  let offset = 0;
+  let l = 1000;
+  let accTxs = [];
+  while (l === 1000) {
+    const rs = await fetchGraphQL(accTxs.length + offset);
+    let txs = [];
+    for (r of rs) {
+      const [a, b] = r["created_at"].split("-");
+      if (a === "2021" && b === "04") {
+        txs = [...txs, [r["created_at"].split("T")[0], r["tx_hash"]]];
+      }
+    }
+    l = txs.length;
+    console.log(accTxs.length, txs[0][0]);
+    accTxs = [...accTxs, ...txs];
+    await sleep(500);
+  }
+
+  console.log(accTxs.length);
+
+  fs.writeFile(
+    network + "_raw_txs.json",
+    JSON.stringify(accTxs),
+    function (err) {
+      if (err) throw err;
+      console.log("Saved Result");
+    }
+  );
+};
+
+(async () => {
+  await graphqlToJson();
+  return;
+
+  const accTxs = JSON.parse(fs.readFileSync(network + "_raw_txs.json"));
+
+  let pairs = [];
+  // start
+  l = 160000;
+  while (true) {
+    try {
+      console.log("pairs:", l, pairs.length);
+      const slice = accTxs.slice(l, l + 100);
+      if (slice.length < 1) {
+        break;
+      }
+
+      const tmp = await Promise.all(
+        slice.map(async (e) => {
+          return [e[0], await getTxInputTerra(e[0], e[1])];
+        })
+      );
+      pairs = [...pairs, ...tmp];
+      l += 100;
+    } catch (e) {
+      console.log(e);
+      break;
+    }
+    await sleep(1000);
+    // if (l % 20_000 === 0) {
+    //   console.log("break if l % 20_000 === 0: ", l);
+    //   break;
+    // }
+  }
+
+  const a = {};
+  for (const p of pairs) {
+    const [date, datas] = p;
+    for (const data of datas) {
+      const [symbol, rate] = data;
+      if (!a[date]) {
+        a[date] = {};
+      }
+      if (!a[date][symbol]) {
+        a[date][symbol] = [];
+      }
+      a[date][symbol] = [...a[date][symbol], rate];
+    }
+  }
+
+  fs.writeFile(network + "_" + l + ".json", JSON.stringify(a), function (err) {
+    if (err) throw err;
+    console.log("Saved Result");
+  });
+
+  fs.writeFile(
+    network + "_" + l + "_tx_not_found.json",
+    JSON.stringify(txNotFound),
+    function (err) {
+      if (err) throw err;
+      console.log("Saved Tx Not Found");
+    }
+  );
+})();
---
+++ report.js
@@ -0,0 +1,93 @@
+const fs = require("fs");
+
+const folder = "mirror_mainnet";
+const header = "mainnet_target_mirror";
+const mids = [
+  "20000",
+  "40000",
+  "60000",
+  "80000",
+  "100000",
+  "120000",
+  "160000",
+  "267900",
+];
+
+const tnfs = (() => {
+  let acc = [];
+  for (const m of mids) {
+    const a = JSON.parse(
+      fs.readFileSync(`${folder}/${header}_${m}_tx_not_found.json`)
+    );
+    acc = [...acc, ...a];
+  }
+  return acc;
+})();
+
+const rs = (() => {
+  let acc = null;
+  for (const m of mids) {
+    if (!acc) {
+      acc = JSON.parse(fs.readFileSync(`${folder}/${header}_${m}.json`));
+    } else {
+      const a = JSON.parse(fs.readFileSync(`${folder}/${header}_${m}.json`));
+      for (const [k, v] of Object.entries(a)) {
+        if (!acc[k]) {
+          acc[k] = v;
+        } else {
+          for (const [kk, vv] of Object.entries(acc[k])) {
+            if (!acc[k][kk]) {
+              acc[k][kk] = vv;
+            } else {
+              acc[k][kk] = [...acc[k][kk], ...vv];
+            }
+          }
+        }
+      }
+    }
+  }
+  return acc;
+})();
+
+const tnfObj = tnfs.reduce((a, b) => {
+  if (!a[b[0]]) {
+    a[b[0]] = 1;
+  } else {
+    a[b[0]] = a[b[0]] + 1;
+  }
+  return a;
+}, {});
+
+const days = Object.keys(rs).sort();
+const symbols = Array.from(
+  Object.values(rs).reduce((s, e) => {
+    for (const k in e) {
+      s.add(k);
+    }
+    return s;
+  }, new Set())
+).sort();
+
+let text = "Date,Fail Txs," + symbols.join() + "\n";
+for (const day of days) {
+  text += day + ",";
+  if (tnfObj[day]) {
+    text += tnfObj[day] + ",";
+  } else {
+    text += "0,";
+  }
+  let aa = [];
+  for (const symbol of symbols) {
+    if (rs[day] && rs[day][symbol]) {
+      aa = [...aa, rs[day][symbol].length];
+    } else {
+      aa = [...aa, 0];
+    }
+  }
+  text += aa.join() + "\n";
+}
+
+fs.writeFile(header + ".csv", text, function (err) {
+  if (err) throw err;
+  console.log("Saved Tx Not Found");
+});
